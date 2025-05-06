97aa44ffa439ebbcbdc1a2c1a0e7f90f9a4e002d
xm20 <glebf72@gmail.com>
2025-05-06 23:49:26 +0300

    Провёл некие фиксы, теперь все работает

--- server.py
+++ server.py
@@ -7,18 +7,22 @@ import hashlib
 
 app = Flask("Server SCES")
 
+
+# Это алгоритм генерации пары ключей RSA
 def gen_rsa_pkey():
-    p, q = int(os.getenv("P_MODULO_PART")), int(os.getenv("Q_MODULO_PART"))
-    phi = (p - 1) * (q - 1)
-    e = number.getPrime(256)
-    d = pow(e, -1, phi)
+    p, q = int(os.getenv("P_MODULO_PART")), int(os.getenv("Q_MODULO_PART")) # p, q - простые части модуля, хранятся в переменных окружения
+    phi = (p - 1) * (q - 1) # Вычисление функции Эйлера
+    e = number.getPrime(256) # Конечно, важно лишь условие об НОД(e, phi) = 1, но, в простых числах жто выполняется
+    d = pow(e, -1, phi) # Поиск обратного по модулю числа
     return e, d
 
 
+# Это общедоступная страничка
+# Нужна для проверки подлинности подписей
+# У сервера есть приватный ключ для проверки подписи
+# Подпись - это хеш персональных данных
 @app.route("/api/check_user", methods=["GET"])
 def check_user():
-    # server has private key to verify signature
-    # signature is encrypted hash of surname
     pubkey = int(request.args.get('pubkey')) # get public key
     uid = request.args.get("uid") # from which user server need to get private key to check signature
     if pubkey is None or uid is None:
@@ -45,6 +49,9 @@ def check_user():
     return "Bad request!", 400
 
 
+
+# Это страничка для администраторов
+# На ней создаются пользователи
 @app.route("/api/set_user", methods=["POST"])
 def set_user():
     data = request.args.get("pdata")
@@ -60,61 +67,69 @@ def set_user():
     cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (hash, str(e), str(d)))
     con.commit()
     con.close()
+
+    con = sqlite3.connect("./db/pairs.db")
+    cursor = con.cursor()
+
+    cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (hash, data))
+    con.commit()
+    con.close()
+
     return str(e), 200
 
 
-@app.route("/api/create_package", methods=["POST"])
-def create_package():
-    global used_packages_uuid
+# Это страница для админов, менеджеров, продавцов
+# Здесь создвются посылки
+@app.route("/api/create_package", methods=["POST"]) 
+def create_package(): # todo: Сделать контроль целостности засчёт сохранения фоток и видео
     parameters = ['weight', 'width', 'height', 'thickness']
     
     for param in parameters:
         if param not in request.args.keys():
             return "Bad request", 400
         
-    while (package_uuid := uuid.uuid1()) in used_packages_uuid:
-        continue
-    
-    used_packages_uuid.add(package_uuid)
+    package_uuid = uuid.uuid1()
 
     con = sqlite3.connect("./db/packages.db")
     cursor = con.cursor()
-    cursor.execute("INSERT INTO packages VALUES(?, ?, ?, ?, ?)", (package_uuid, float(request.weight), int(request.width), int(request.height), int(request.thickness)))
+    cursor.execute("INSERT INTO packages (id, weight, width, height, thinkness) VALUES (?, ?, ?, ?, ?)", (str(package_uuid), float(request.args.get('weight')), int(request.args.get('width')), int(request.args.get('height')), int(request.args.get('thickness'))))
     con.commit()
     con.close()
 
     return f"Package created, uid: {package_uuid}", 200
     
 
-
+# Страничка служебная, для удаления посылок.
+# Когда посылка доставлена, она удаляется из всех бд.
 @app.route("/api/remove_package")
 def remove_package():
-    if request.uid is None:
-        return "Bad request"
-    
-    global used_packages_uuid
+    if 'uid' not in request.args.keys():
+        return "Bad request", 400
 
     con = sqlite3.connect("./db/packages.db")
     cursor = con.cursor()
-    cursor.execute("DELETE FROM packages WHERE id=?", (request.uid, )).fetchone()
+    cursor.execute("DELETE FROM packages WHERE id=?", (str(request.args.get('uid')), )).fetchone()
     con.commit()
-
-    used_packages_uuid.pop(request.uid)
 
     return "Package deleted", 200
 
 
+# Это страничка для админов.
+# Репорты - специальная жалоба сотрудника, если что-то пошло не так при передаче посылки
+@app.route("/reports")
+def reports():
+    pass # todo: придумать и написать сохранение репортов
 
-used_packages_uuid = set()
 
 @app.route("/")
 def main():
     return "It works!"
 
+
 os.environ['Q_MODULO_PART'] = str(number.getPrime(1024))
 os.environ['P_MODULO_PART'] = str(number.getPrime(1024))
 
-if not os.path.isfile('./db'):
+if not os.path.isdir('./db'):
     os.mkdir("./db")
 
 if not os.path.isfile('./db/users.db'):
